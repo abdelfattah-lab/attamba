1835825144aaba24fe6c4e40f65fc42e1d827577
Yash Akhauri <akhauri.yash@gmail.com>
2024-11-19 18:02:12 -0500

    new code- backup

--- collated_wk2_gen.py
+++ collated_wk2_gen.py
@@ -15,12 +15,16 @@ from matplotlib import cycler
 
 
 name_dict = {
-    "AttentiveSSMNoProjCyc8L16_Long": "Attamba\u00A0\u00A0(512\u00A0 8\u00A0 16\u00A0 \u00A0\u00A0\u00A0\u00A032\u00A0 \u00A0\u00A0\u00A01\u00A0 \u00A0\u00A08)",
+    # "AttentiveSSMNoProjCyc8L16_Long_NoRes": "Attamba\u00A0\u00A0(512\u00A0 8\u00A0 16\u00A0 \u00A0\u00A0\u00A0\u00A032\u00A0 \u00A0\u00A0\u00A01\u00A0 \u00A0\u00A08)",
+
+    "AttentiveSSMNoProjCyc4L32_Long": "Attamba\u00A0\u00A0(512\u00A0 4\u00A0 32\u00A0 \u00A0\u00A0\u00A0\u00A032\u00A0 \u00A0\u00A0\u00A01\u00A0 \u00A0\u00A08)",
     "xmer_long": "Xmer\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0(512\u00A0 0\u00A0 \u00A0\u00A00\u00A0 \u00A0\u00A0\u00A0\u00A0\u00A0\u00A00\u00A0 \u00A0\u00A0\u00A00\u00A0 \u00A0\u00A08)",
     "mamba_long": "Mamba\u00A0\u00A0\u00A0\u00A0(640\u00A0 0\u00A0 \u00A0\u00A00\u00A0 \u00A0\u00A0\u00A0\u00A064\u00A0 \u00A0\u00A0\u00A01\u00A0 \u00A0\u00A08)",
     
     "mamba_long_v2": "Mamba\u00A0\u00A0\u00A0\u00A0(512\u00A0 0\u00A0 \u00A0\u00A00\u00A0 1024\u00A0 \u00A0\u00A0\u00A02\u00A0 \u00A0\u00A08)",
-    "AttentiveSSMNoProjCyc4L32_Long": "Attamba\u00A0\u00A0(512\u00A0 4\u00A0 32\u00A0 \u00A0\u00A0\u00A0\u00A032\u00A0 \u00A0\u00A0\u00A01\u00A0 \u00A0\u00A08)",
+    # "AttentiveSSMNoProjCyc8L16_Long_NoRes": "Attamba\u00A0\u00A0(512\u00A0 8\u00A0 16\u00A0 \u00A0\u00A0\u00A0\u00A032\u00A0 \u00A0\u00A0\u00A01\u00A0 \u00A0\u00A08)",
+
+    "AttentiveSSMNoProjCyc8L16_Long": "Attamba\u00A0\u00A0(512\u00A0 8\u00A0 16\u00A0 \u00A0\u00A0\u00A0\u00A032\u00A0 \u00A0\u00A0\u00A01\u00A0 \u00A0\u00A08)",
     "mamba_longrun": "Mamba\u00A0\u00A0\u00A0\u00A0(512\u00A0 0\u00A0 \u00A0\u00A00\u00A0 \u00A0\u00A0128\u00A0 \u00A016\u00A0 16)",
 }
 
@@ -61,6 +65,7 @@ def plot_results(directory, selected_experiments=None):
     experiments = [exp for exp in expt_order if exp in experiments]
 
     plt.figure(figsize=(10, 6))
+    # plt.figure(figsize=(18, 6))
     plt.title("Comparing Transformers, Mamba and Attamba", fontsize=24)
     plt.xlabel("Global Step", fontsize=24)
     plt.ylabel("WK2 Perplexity", fontsize=24)
@@ -84,8 +89,9 @@ def plot_results(directory, selected_experiments=None):
 
     plt.legend(title = "\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0E\u00A0\u00A0\u00A0 C \u00A0\u00A0\u00A0L \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0Ds \u00A0\u00A0\u00A0G \u00A0\u00A0\u00A0H\u00A0", title_fontsize=16, fontsize=16, ncol=1, loc="upper right")
     plt.tight_layout()
+    # plt.xlim(70000, 100000)
     plt.xlim(60000, 100000)
-    plt.ylim(20, 30)
+    plt.ylim(20, 32)
     plt.savefig("collated_results.pdf")
     print("Plot saved as 'collated_results.pdf'")
 
