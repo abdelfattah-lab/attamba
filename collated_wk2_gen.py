4c3f55e4993be191740ebfe78ec13f923f52a2ae
Yash Akhauri <akhauri.yash@gmail.com>
2024-11-20 09:25:19 -0500

    before FAttn fix attemtp

--- collated_wk2_gen.py
+++ collated_wk2_gen.py
@@ -3,6 +3,7 @@ import json
 import argparse
 import matplotlib.pyplot as plt
 from matplotlib import cycler
+from matplotlib.ticker import ScalarFormatter
 # name_dict = {
 #     "AttentiveSSMNoProjCyc8L16_Long": "Attamba\u00A0\u00A0(512, 8, 16, \u00A0\u00A0\u00A0\u00A032, \u00A0\u00A0\u00A01, \u00A0\u00A08)",
 #     "xmer_long": "Xmer\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0(512, 0, \u00A0\u00A00, \u00A0\u00A0\u00A0\u00A0\u00A0\u00A00, \u00A0\u00A0\u00A00, \u00A0\u00A08)",
@@ -26,12 +27,15 @@ name_dict = {
 
     "AttentiveSSMNoProjCyc8L16_Long": "Attamba\u00A0\u00A0(512\u00A0 8\u00A0 16\u00A0 \u00A0\u00A0\u00A0\u00A032\u00A0 \u00A0\u00A0\u00A01\u00A0 \u00A0\u00A08)",
     "mamba_longrun": "Mamba\u00A0\u00A0\u00A0\u00A0(512\u00A0 0\u00A0 \u00A0\u00A00\u00A0 \u00A0\u00A0128\u00A0 \u00A016\u00A0 16)",
+    "hawk": "Hawk\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0(480\u00A0 0\u00A0 \u00A0\u00A00\u00A0 \u00A0\u00A0\u00A0\u00A0\u00A0\u00A00\u00A0 \u00A0\u00A0\u00A00\u00A0 \u00A0\u00A08)",
+    "minGRU": "minGRU\u00A0\u00A0\u00A0(576\u00A0 0\u00A0 \u00A0\u00A00\u00A0 \u00A0\u00A0\u00A0\u00A0\u00A0\u00A00\u00A0 \u00A0\u00A0\u00A00\u00A0 \u00A0\u00A08)",
 }
 
 skip_expts = [""] # These are lower params, so skip them -- actually log differs so its ok.
 
 
-expt_order = ["AttentiveSSMNoProjCyc8L16_Long", "AttentiveSSMNoProjCyc4L32_Long", "mamba_long", "mamba_longrun", "mamba_long_v2", "xmer_long"]
+# expt_order = ["AttentiveSSMNoProjCyc8L16_Long", "AttentiveSSMNoProjCyc4L32_Long", "mamba_long", "mamba_longrun", "mamba_long_v2", "minGRU", "hawk", "xmer_long"]
+expt_order = ["minGRU", "hawk", "mamba_longrun", "AttentiveSSMNoProjCyc8L16_Long", "mamba_long", "mamba_long_v2", "AttentiveSSMNoProjCyc4L32_Long", "xmer_long"]
 colors = plt.cm.tab20.colors  # Use a colormap for more distinct colors
 markers = ['o', '<', '*', 'h', 'X', 'P', '|', '+', 's', 'D', '^', 'v',  '>', 'p', 'x']
 
@@ -53,20 +57,26 @@ def load_metrics_from_folder(folder_path):
             data = json.loads(line)
             steps.append(data["global_step"])
             perplexities.append(data["wikitext"]["word_perplexity,none"])
-    
-    return steps, perplexities
+    final_perplexity = perplexities[-1] if perplexities else None
+
+    return steps, perplexities, final_perplexity
 
 def plot_results(directory, selected_experiments=None):
     """Plots the perplexity values for all or selected experiments"""
     experiments = os.listdir(directory)
-    if selected_experiments:
-        experiments = [exp for exp in experiments if exp in selected_experiments]
+    # if selected_experiments:
+    #     experiments = [exp for exp in experiments if exp in selected_experiments]
+    print(experiments)
+    # experiments = [exp for exp in expt_order if exp in experiments]
+    experiments = [exp for exp in experiments if exp in expt_order]
+    experiments = sorted(experiments, key=lambda exp: expt_order.index(exp))
 
-    experiments = [exp for exp in expt_order if exp in experiments]
+    # remove from experiments if not in expt_order
+    print(experiments)
 
     plt.figure(figsize=(10, 6))
     # plt.figure(figsize=(18, 6))
-    plt.title("Comparing Transformers, Mamba and Attamba", fontsize=24)
+    plt.title("Transformer, Mamba, minGRU, Hawk, Attamba", fontsize=24)
     plt.xlabel("Global Step", fontsize=24)
     plt.ylabel("WK2 Perplexity", fontsize=24)
     plt.grid(visible=True, linestyle="--", alpha=0.5)
@@ -75,23 +85,32 @@ def plot_results(directory, selected_experiments=None):
 
     for experiment in experiments:
         if experiment in skip_expts:
+            print("SKIP ", experiment)
             continue
+        print(experiment)
         folder_path = os.path.join(directory, experiment)
         if os.path.isdir(folder_path):
-            steps, perplexities = load_metrics_from_folder(folder_path)
+            steps, perplexities, fpplx = load_metrics_from_folder(folder_path)
             if steps and perplexities:
                 # Use name_dict to change experiment name if it exists in the dictionary
                 plot_label = name_dict.get(experiment, experiment)
+                if fpplx is not None:
+                    plot_label += f"\u00A0\u00A0\u00A0\u00A0{fpplx:.2f}"
                 
                 plt.plot(steps, perplexities, label=plot_label, linestyle="-", 
                          color=colors[marker_index % num_combinations], markersize=12, marker=markers[marker_index % len(markers)])
                 marker_index += 1  # Increment marker index
 
-    plt.legend(title = "\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0E\u00A0\u00A0\u00A0 C \u00A0\u00A0\u00A0L \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0Ds \u00A0\u00A0\u00A0G \u00A0\u00A0\u00A0H\u00A0", title_fontsize=16, fontsize=16, ncol=1, loc="upper right")
+    plt.legend(title = "\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0E\u00A0\u00A0\u00A0 C \u00A0\u00A0\u00A0L \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0Ds \u00A0\u00A0\u00A0G \u00A0\u00A0\u00A0H\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u2003\u2002\u2009\u200A", \
+                        title_fontsize=16, fontsize=16, ncol=1, loc="lower left")
+    plt.tick_params(axis='both', which='major', labelsize=20)
     plt.tight_layout()
-    # plt.xlim(70000, 100000)
-    plt.xlim(60000, 100000)
-    plt.ylim(20, 32)
+    plt.gca().set_xticks([20000, 40000, 60000, 80000, 100000])  # Multiples of 10,000
+    plt.gca().set_yticks([22, 24, 26, 28])  # Multiples of 10,000
+    # plt.xlim(40000, 100000)
+    # plt.ylim(20.5, 34)
+    plt.xlim(20000, 100000)
+    plt.ylim(20.5, 28)
     plt.savefig("collated_results.pdf")
     print("Plot saved as 'collated_results.pdf'")
 
--- reported_ablations.py
+++ reported_ablations.py
@@ -3,12 +3,13 @@ import json
 import argparse
 import matplotlib.pyplot as plt
 from matplotlib import cycler
+from matplotlib.ticker import ScalarFormatter
 
 # Define colors and markers
 colors = plt.cm.tab20.colors  # Use a colormap for distinct colors
 # markers = ['o', 's', 'D', '^', 'v', '<', '>', 'p', '*', 'h', 'X', 'P', '|', '+', 'x']
 markers = ['o', '<', '*', 'h', 'X', 'P', '|', '+', 's', 'D', '^', 'v',  '>', 'p', 'x']
-
+glabs = 20
 
 num_combinations = min(len(colors), len(markers))
 
@@ -31,7 +32,7 @@ def plot_experiment_group_7(directory, experiment_names, file_name):
         label = get_label(experiment, 7)
         folder_path = os.path.join(directory, experiment)
         if os.path.isdir(folder_path):
-            steps, wk2_perplexity = load_metrics_from_folder(folder_path)
+            steps, wk2_perplexity, fpplx = load_metrics_from_folder(folder_path)
             if steps and wk2_perplexity:
                 final_perplexity = wk2_perplexity[-1]
 
@@ -65,6 +66,8 @@ def plot_experiment_group_7(directory, experiment_names, file_name):
 
     plt.legend(fontsize=24, loc="upper right")
     plt.title("Impact of Full-Attention on Leading L Tokens", fontsize=24)
+    plt.tick_params(axis='both', which='major', labelsize=glabs)
+
     plt.tight_layout()
     # plt.yscale("log")
     plt.xscale("log")
@@ -88,15 +91,17 @@ def load_metrics_from_folder(folder_path):
             data = json.loads(line)
             steps.append(data["global_step"])
             perplexities.append(data["wikitext"]["word_perplexity,none"])
+    final_perplexity = perplexities[-1] if perplexities else None
     
-    return steps, perplexities
+    return steps, perplexities, final_perplexity
+
 
 def get_label(experiment_name, experiment_group):
     """Maps experiment names to their formatted labels, specific to each group"""
     if experiment_group == 1:
         mapping = {
-            "AttentiveSSMWProjUnif": "Attamba + Proj",
-            "AttentiveSSMNoProjUnif": "Attamba - Proj"
+            "AttentiveSSMWProjUnif": "AttambaProj",
+            "AttentiveSSMNoProjUnif": "Attamba\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0"
         }
     elif experiment_group == 2:
         mapping = {
@@ -106,23 +111,23 @@ def get_label(experiment_name, experiment_group):
         }
     elif experiment_group == 3:
         mapping = {
-            "AttentiveSSMNoProjRand": "Random",
-            "AttentiveSSMNoProjUnif": "Uniform",
-            "AttentiveSSMNoProjFSSM": "FSSM",
-            "AttentiveSSMNoProjFAttn": "FAttn",
-            "AttentiveSSMNoProjCyc": "Cyclic",
+            "AttentiveSSMNoProjRand": "Random\u2006",
+            "AttentiveSSMNoProjUnif": "Uniform\u00A0",
+            "AttentiveSSMNoProjFSSM": "FSSM\u00A0\u2003\u00A0",
+            "AttentiveSSMNoProjFAttn": "FAttn\u00A0\u2003\u2002",
+            "AttentiveSSMNoProjCyc": "Cyclic\u00A0\u2003",
         }
     elif experiment_group == 4:
         mapping = {
-            "AttentiveSSMNoProjCyc4": "Attamba C4\u00A0\u00A0\u00A0\u00A0\u00A0L4",
-            "AttentiveSSMNoProjCyc": "Attamba C8\u00A0\u00A0\u00A0\u00A0\u00A0L8",
-            "AttentiveSSMNoProjCyc64": "Attamba C64\u00A0\u00A0\u00A0L64",
+            "AttentiveSSMNoProjCyc4": "Attamba C4\u00A0\u00A0\u00A0\u00A0\u00A0L4\u2007\u2007",
+            "AttentiveSSMNoProjCyc": "Attamba C8\u00A0\u00A0\u00A0\u00A0\u00A0L8\u2007\u2007",
+            "AttentiveSSMNoProjCyc64": "Attamba C64\u00A0\u00A0\u00A0L64\u2007",
             "AttentiveSSMNoProjCyc128": "Attamba C128 L128"
         }
     elif experiment_group == 5:
         mapping = {
-            "xmer_100k_dclm": "Transformer",
-            "AttentiveSSMNoProjCyc": "Attamba\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0C8\u00A0\u00A0\u00A0\u00A0 L8",
+            "xmer_100k_dclm": "Transformer\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u2007\u2007\u2007\u2002\u2002\u2005",
+            "AttentiveSSMNoProjCyc": "Attamba\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0C8\u00A0\u00A0\u00A0\u00A0 L8\u2007\u2007\u2007\u2006",
             "AttentiveSSMNoProjCycPseudo": "Pseudo Attamba C8\u00A0\u00A0\u00A0\u00A0 L1024",
             "AttentiveSSMNoProjCycPseudo128": "Pseudo Attamba C128 L1024"
         }
@@ -165,9 +170,11 @@ def plot_experiment(directory, experiment_names, file_name, experiment_group):
     for experiment in experiment_names:
         folder_path = os.path.join(directory, experiment)
         if os.path.isdir(folder_path):
-            steps, perplexities = load_metrics_from_folder(folder_path)
+            steps, perplexities, fpplx = load_metrics_from_folder(folder_path)
             if steps and perplexities:
                 label = get_label(experiment, experiment_group)
+                if fpplx is not None:
+                    label += f"\u00A0\u00A0\u00A0\u00A0{fpplx:.1f}"
                 plt.plot(steps, perplexities, label=label, linestyle="-", 
                          color=colors[marker_index % num_combinations], marker=markers[marker_index % len(markers)],
                          markersize=12)
@@ -182,32 +189,42 @@ def plot_experiment(directory, experiment_names, file_name, experiment_group):
         plt.xscale("log")
         # plt.yscale("log")
         # plt.xscale("log")
+        plt.tick_params(axis='x', which='major', labelsize=glabs)
+        plt.tick_params(axis='x', which='both', labelsize=glabs)
+        plt.gca().xaxis.set_major_formatter(ScalarFormatter())
         plt.title("Impact of KV-Projection before SSM", fontsize=24)
     if "expt_2" in file_name:
         plt.xlim(40000, 60000)
         # Set upper ylim to 60 without setting lower limit
         plt.ylim(34, 44)
-        # plt.yscale("log")
-        # plt.xscale("log")
-        # plt.yscale("log")
-        # plt.xscale("log")
+        plt.gca().set_xticks([40000, 50000, 60000])  # Multiples of 10,000
+        plt.tick_params(axis='x', which='major', labelsize=glabs)
         plt.title("Impact of Increasing SSM State-Dimension", fontsize=24)
-        # plt.yscale("log")
-        # plt.xscale("log")
     if "expt_3" in file_name:
         plt.title("Impact of Chunking Boundary Strategy", fontsize=24)
         plt.yscale("log")
         plt.xscale("log")
+        # set y ticks as [40, 60, 100]
+        plt.gca().set_yticks([40, 60, 100])
+        plt.gca().get_yaxis().set_major_formatter(plt.ScalarFormatter(useOffset=False))
+        plt.tick_params(axis='y', which='major', labelsize=glabs)
+        plt.tick_params(axis='y', which='both', labelsize=glabs)
     if "expt_4" in file_name:
         plt.xlim(10000)
         plt.title("Impact of Chunk Size", fontsize=24)
         plt.yscale("log")
+        plt.gca().set_yticks([40, 60, 100])
+        plt.gca().get_yaxis().set_major_formatter(plt.ScalarFormatter(useOffset=False))
+        plt.tick_params(axis='both', which='both', labelsize=glabs)
         plt.xscale("log")
     if "expt_5" in file_name:
         plt.title("Impact of Pseudo-Chunking", fontsize=24)
         plt.xlim(10000)
         # Set upper ylim to 60 without setting lower limit
         plt.ylim(top=50)
+        plt.legend(fontsize=20, loc="lower left")
+        plt.tick_params(axis='x', which='major', labelsize=glabs)
+        plt.tick_params(axis='x', which='both', labelsize=glabs)
         # plt.yscale("log")
         plt.xscale("log")
     if "expt_6" in file_name:
@@ -217,6 +234,7 @@ def plot_experiment(directory, experiment_names, file_name, experiment_group):
         # change legend location to top left
         plt.legend(fontsize=14, loc="lower left")
         plt.yscale("log")
+    plt.tick_params(axis='both', which='major', labelsize=glabs)
     plt.tight_layout()
     os.makedirs("reported_graphs", exist_ok=True)
     plt.savefig(os.path.join("reported_graphs", file_name))
